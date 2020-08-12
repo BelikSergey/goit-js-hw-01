e399dcd6e44d5105891e455e6df3265f03ed47c6
BelikSergey <65306747+BelikSergey@users.noreply.github.com>
2020-08-12 21:41:34 +0300

    добавляет решение первой задачи

---
+++ JS/task-1.js
@@ -0,0 +1,8 @@
+"use strict";
+const name = '"Генератор защитного поля"';
+let price = 1000;
+const message = `Выбран ${name}, цена за штуку ${price} кредитов`;
+console.log(message);
+price = 2000;
+const messageSecond = `Выбран ${name}, цена за штуку ${price} кредитов`;
+console.log(messageSecond);
